4b6e02721de1853417f6e0393b1d66d068965d2b
c18441084 <niallmcnamara1999@gmail.com>
2026-06-15 20:25:34 +0100

    attempting to fix imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Home from './components/Niallmcnamara.jsx';
+import Home from './components/Niallmcnamara';
 
 function App() {
   const [count, setCount] = useState(0)
